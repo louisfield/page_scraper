5b83f99f47de1a6c1a009e5d9ce7b5bebe51360a
Louis <lf00341@surrey.ac.uk>
2021-05-11 20:53:52 +0100

    A* fixed, UCAS Points implemented

--- src/main/java/com/pagescraper/PageScraper.java
+++ src/main/java/com/pagescraper/PageScraper.java
@@ -8,17 +8,17 @@ import org.jsoup.safety.Whitelist;
 
 import java.net.URLEncoder;
 import java.util.*;
-
+import java.util.stream.Collectors;
 public class PageScraper {
     public static final Map<String, Integer> GRADES = mapinit();
     private static Map<String, Integer> mapinit(){
         Map<String,Integer> map = new HashMap<String,Integer>();
-        map.put("A",56);
-        map.put("B",48);
-        map.put("C",40);
-        map.put("D",32);
-        map.put("E",24);
-        map.put("F",16);
+        map.put("Z",56);
+        map.put("A",48);
+        map.put("B",40);
+        map.put("C",32);
+        map.put("D",24);
+        map.put("E",16);
         return Collections.unmodifiableMap(map);
     }
 
@@ -30,7 +30,7 @@ public class PageScraper {
         webClient.getOptions().setCssEnabled(false);
         webClient.getOptions().setJavaScriptEnabled(false);
         try{
-            String searchUrl = "https://www.ucl.ac.uk/chemistry/study-here/undergraduate-degrees/bsc-chemistry/chemistry-bsc";//+ URLEncoder.encode(search_test, "UTF-8");
+            String searchUrl = "https://www.undergraduate.study.cam.ac.uk/courses/computer-science";//+ URLEncoder.encode(search_test, "UTF-8");
             HtmlPage page = webClient.getPage(searchUrl);
             //System.out.println(page.toString());
             WebResponse response = page.getWebResponse();
@@ -42,21 +42,42 @@ public class PageScraper {
 
             System.out.println(safeSplit[0]);
             for (String sub : safeSplit) {
-                if(sub.contains("AAA") || sub.contains("AAB") || sub.contains("ABB") || sub.contains("BBB") || sub.contains("BBC") || sub.contains("BCC") || sub.contains("CCC")){
-
+                if( sub.contains("AAA") || sub.contains("AAB") || sub.contains("ABB") || sub.contains("BBB") || sub.contains("BBC") || sub.contains("BCC") || sub.contains("CCC")){
+                    System.out.println(sub);
                     requirementSeen.add(sub.replace("&nbsp;"," ").replace("</p>","").replaceAll("/[^a-zA-Z ]/g", ""));
+                } else if(sub.contains("A*A*A*")){
+                    requirementSeen.add("ZZZ");
+                } else if(sub.contains("A*A*A")){
+                    requirementSeen.add("ZZA");
+                } else if(sub.contains("A*AA")){
+                    requirementSeen.add("ZAA");
                 }
             }
-            ArrayList<String> requirementFinal = new ArrayList<String>();
+            List<String> requirementFinal = new ArrayList<String>();
             for (String res : requirementSeen) {
                 String[] result = res.split(" ");
                 for (String word: result){
-                    if(word.length() == 3 && word.toUpperCase().equals(word)) {
+
+                    if(word.length() < 7 && word.toUpperCase().equals(word) && !word.contains("C") && !word.contains("U") && !word.contains("L") & !word.matches("[0-9]+")) {
                         requirementFinal.add(word);
                     }
                 }
             }
+            ArrayList<Integer> finalvals = new ArrayList<Integer>();
+            for (String val : requirementFinal) {
+                String[] grades = val.split("");
+                int finalval = 0;
+                for (String grade : grades){
+                    if(GRADES.containsKey(grade)){
+                        finalval += GRADES.get(grade);
+                    }
+                }
+                finalvals.add(finalval);
+            }
+
+
             System.out.println(requirementFinal);
+            System.out.println(finalvals);
 
         } catch (Exception e){
             e.printStackTrace();
