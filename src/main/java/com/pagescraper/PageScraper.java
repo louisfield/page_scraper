06d3ccc00bb236f4e95102468951138d360c6ace
Louis <lf00341@surrey.ac.uk>
2021-05-12 11:16:35 +0100

    Google search for University requirements

--- src/main/java/com/pagescraper/PageScraper.java
+++ src/main/java/com/pagescraper/PageScraper.java
@@ -4,8 +4,12 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -23,66 +27,81 @@ public class PageScraper {
     }
 
     public static void main(String[] args) {
-        String search_test = "Android";
+        String search_test = "Computer Science University Requirements";
 
 
         WebClient webClient = new WebClient();
         webClient.getOptions().setCssEnabled(false);
         webClient.getOptions().setJavaScriptEnabled(false);
-        try{
-            String searchUrl = "https://www.ox.ac.uk/admissions/undergraduate/courses-listing/computer-science";//+ URLEncoder.encode(search_test, "UTF-8");
-            HtmlPage page = webClient.getPage(searchUrl);
-            //System.out.println(page.toString());
-            WebResponse response = page.getWebResponse();
-            Whitelist whitelist = Whitelist.none();
-            whitelist.addTags(new String[]{"p", "ul"});
-            String safe = Jsoup.clean(response.getContentAsString(), whitelist);
-            String[] safeSplit = safe.split("<p>");
-            ArrayList<String> requirementSeen = new ArrayList<String>();
-
-            System.out.println(safeSplit[0]);
-            for (String sub : safeSplit) {
-                if( sub.contains("AAA") || sub.contains("AAB") || sub.contains("ABB") || sub.contains("BBB") || sub.contains("BBC") || sub.contains("BCC") || sub.contains("CCC")){
-                    System.out.println(sub);
-                    requirementSeen.add(sub.replace("&nbsp;"," ").replace("</p>","").replaceAll("/[^a-zA-Z ]/g", ""));
-                } else if(sub.contains("A*A*A*")){
-                    requirementSeen.add("ZZZ");
-                } else if(sub.contains("A*A*A")){
-                    requirementSeen.add("ZZA");
-                } else if(sub.contains("A*AA")){
-                    requirementSeen.add("ZAA");
-                }
-            }
-            ArrayList<String> requirementFinal = new ArrayList<String>();
-            for (String res : requirementSeen) {
-                String[] result = res.split(" ");
-                for (String word: result){
-
-                    if(word.length() == 3 && word.toUpperCase().equals(word) && !word.contains("C") && !word.contains("U") && !word.contains("L") & !word.matches("[0-9]+")) {
-                        requirementFinal.add(word);
-                    }
-                }
-            }
-            ArrayList<Integer> finalvals = new ArrayList<Integer>();
-            for (String val : requirementFinal) {
-                String[] grades = val.split("");
-                int finalval = 0;
-                for (String grade : grades){
-                    if(GRADES.containsKey(grade)){
-                        finalval += GRADES.get(grade);
-                    }
-                }
-                finalvals.add(finalval);
-                requirementFinal.set(requirementFinal.indexOf(val),val.replace("Z","A*"));
-            }
-
-
-            System.out.println(requirementFinal);
-            System.out.println(finalvals);
-
-        } catch (Exception e){
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("http://google.com/search?q=" + search_test).userAgent("Mozilla").get();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Elements links = doc.select("a[href]");
+        for(Element el: links){
+            try{
+
+
+                String searchUrl = el.attr("abs:href");;;//+ URLEncoder.encode(search_test, "UTF-8");
+                HtmlPage page = webClient.getPage(searchUrl);
+                //System.out.println(page.toString());
+                WebResponse response = page.getWebResponse();
+                Whitelist whitelist = Whitelist.none();
+                whitelist.addTags(new String[]{"p", "ul"});
+                String safe = Jsoup.clean(response.getContentAsString(), whitelist);
+                String[] safeSplit = safe.split("<p>");
+                ArrayList<String> requirementSeen = new ArrayList<String>();
+
+
+                for (String sub : safeSplit) {
+                    sub = sub.replace("&nbsp;"," ").replace("</p>","").replaceAll("/[^a-zA-Z ]/g", "").replace("'","");
+                    if( sub.contains("AAA") || sub.contains("AAB") || sub.contains("ABB") || sub.contains("BBB") || sub.contains("BBC") || sub.contains("BCC") || sub.contains("CCC")){
+
+                        requirementSeen.add(sub);
+                    } else if(sub.contains("A*A*A*")){
+                        requirementSeen.add("ZZZ");
+                    } else if(sub.contains("A*A*A")){
+                        requirementSeen.add("ZZA");
+                    } else if(sub.contains("A*AA")){
+                        requirementSeen.add("ZAA");
+                    }
+                }
+                ArrayList<String> requirementFinal = new ArrayList<String>();
+                for (String res : requirementSeen) {
+                    String[] result = res.split(" ");
+                    for (String word: result){
+
+                        if(word.length() == 3 && word.toUpperCase().equals(word) && (word.contains("Z")  || word.contains("A") || word.contains("B") || word.contains("C") ) && !word.matches("[0-9]+")) {
+                            requirementFinal.add(word);
+                        }
+                    }
+                }
+                ArrayList<Integer> finalvals = new ArrayList<Integer>();
+                for (String val : requirementFinal) {
+                    String[] grades = val.split("");
+                    int finalval = 0;
+                    for (String grade : grades){
+                        if(GRADES.containsKey(grade)){
+                            finalval += GRADES.get(grade);
+                        }
+                    }
+                    finalvals.add(finalval);
+                    requirementFinal.set(requirementFinal.indexOf(val),val.replace("Z","A*"));
+                }
+
+                if(!requirementFinal.isEmpty() && !finalvals.isEmpty()) {
+                    System.out.println(requirementFinal);
+                    System.out.println(finalvals);
+                }
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
 
 
     }
